e3f7e99242ceab731e6991e01ca752641dd2438c
calebanderson <caleb.r.anderson.1@gmail.com>
2022-11-30 22:40:15 -0500

    Added initialization to railtie

--- lib/scrat/railtie.rb
+++ lib/scrat/railtie.rb
@@ -1,4 +1,12 @@
 module Scrat
   class Railtie < ::Rails::Railtie
+    config.after_initialize do
+      Object.extend(Scrat::Context)
+      irb_main.extend(Scrat::Context)
+
+      ReloaderHooks.register do
+        current_workspace.main.submerge if current_workspace.main.is_a?(Pad)
+      end
+    end
   end
 end
